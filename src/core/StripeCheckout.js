94cdb7ea698944bb280a64797ea385a59e45ae33
panayappan01 <77457714+panayappan01@users.noreply.github.com>
2021-01-15 22:19:09 +0530

    cleaning

--- src/core/StripeCheckout.js
+++ src/core/StripeCheckout.js
@@ -33,65 +33,7 @@ const StripeCheckout = ({
     return amount;
   };
 
-  //   const displayRazorpay = async () => {
-  //     // const res = await loadScript(
-  //     //   "https://checkout.razorpay.com/v1/checkout.js"
-  //     // );
-  //     const res;
-
-  //     if (!res) {
-  //       alert("Razorpay SDK failed to load. Are you online?");
-  //       return;
-  //     }
-
-  //     const result = await axios.post(`${API}/orders`);
-
-  //     if (!result) {
-  //       alert("Server error. Are you online?");
-  //       return;
-  //     }
-
-  //     console.log("RESULT.DATA", result.data);
-
-  //     const { amount, id: order_id, currency } = result.data;
-
-  //     const options = {
-  //       key: "rzp_test_FMeqdDQCalKdzX", // Enter the Key ID generated from the Dashboard
-  //       amount: amount.toString(),
-  //       currency: currency,
-  //       name: "Soumya Corp.",
-  //       description: "Test Transaction",
-  //       //   image: { logo },
-  //       order_id: result.data.id,
-  //       handler: async function (response) {
-  //         // const data = {
-  //         //   orderCreationId: order_id,
-  //         //   razorpayPaymentId: response.razorpay_payment_id,
-  //         //   razorpayOrderId: response.razorpay_order_id,
-  //         //   razorpaySignature: response.razorpay_signature,
-  //         // };
-  //         // const result = await axios.post(
-  //         //   "http://localhost:8000/api/success",
-  //         //   data
-  //         // );
-  //         // alert(result.data.msg);
-  //       },
-  //       prefill: {
-  //         name: "Soumya Dey",
-  //         email: "SoumyaDey@example.com",
-  //         contact: "9999999999",
-  //       },
-  //       notes: {
-  //         address: "Soumya Dey Corporate Office",
-  //       },
-  //       theme: {
-  //         color: "#61dafb",
-  //       },
-  //     };
-
-  //     const paymentObject = new window.Razorpay(options);
-  //     paymentObject.open();
-  //   };
+  
 
   const getPaymentDetail = () => {
     getOrderData(getFinalAmount() * 100, token).then((data) => {
